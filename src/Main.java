1370eb9d644b18b5fda65b17b78c6237fff1f266
Cristi Vasile <ing_vasile_cristian@yahoo.co.uk>
2022-10-10 16:14:08 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello\nCristi");
+       // System.out.println("Cristi");
+        int Suma = 3+4;
+        System.out.println(Suma);
+        float impa = 7 / 4f;
+        System.out.println(impa);
+        int sumaa = -5 + 8*6;
+        System.out.println(sumaa);
+        int sumab = (55+9)%9;
+        System.out.println(sumab);
+        float sumac = 20+-3*5/8f;
+        System.out.println(sumac);
+        float sumad = 5+15/3*2-8%3f;
+        System.out.println(sumad);
+
+    }
+}
